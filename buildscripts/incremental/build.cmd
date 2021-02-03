70ce5f3d18699d5f6c34a71709a7504bfbf87ef2
AiHimmel <15845840+Ai-Himmel@users.noreply.github.com>
2021-02-03 22:06:19 +0800

    Update build.cmd

--- buildscripts/incremental/build.cmd
+++ buildscripts/incremental/build.cmd
@@ -3,7 +3,7 @@ call activate %CONDA_ENV%
 
 @rem Build numba extensions without silencing compile errors
 python setup.py build_ext -q --inplace
-
+python setup.py bdist_wheel
 @rem Install numba locally for use in `numba -s` sys info tool at test time
 python -m pip install -e .
 
